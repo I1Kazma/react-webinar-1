326dfa4e49b44afd3a0ce866323dffbde313f501
I1Kazma <117230564+I1Kazma@users.noreply.github.com>
2023-05-13 18:50:47 +0200

    Update app.js 2.0

--- src/app.js
+++ src/app.js
@@ -28,7 +28,7 @@ function App({store}) {
                 <div className='Item-code'>{item.code}</div>
                 <div className='Item-title'>
                     {item.title} 
-                    <span className='Item-select'>{item.selectCount ? `Выделено раз: ${item.selectCount} ` : ''}</span>
+                    <span className='Item-select'>{item.selectCount ? ` | Выделяли ${item.selectCount} ${declOfNum(item.selectCount, ['раз', 'раза'])}` : ''}</span>
                 </div>
                 <div className='Item-actions'>
                   <button onClick={() => store.deleteItem(item.code)}>
