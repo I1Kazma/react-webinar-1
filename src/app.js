ad8e50fe54ad6f80d0225f7d1f240ec4496c10c0
I1Kazma <117230564+I1Kazma@users.noreply.github.com>
2023-05-19 14:43:34 +0200

    Update app.js

--- src/app.js
+++ src/app.js
@@ -9,32 +9,47 @@ import PageLayout from "./components/page-layout";
  * @param store {Store} Хранилище состояния приложения
  * @returns {React.ReactElement}
  */
-function App({store}) {
-
-  const list = store.getState().list;
-
+function App({ store }) {
+  const state = store.getState();
+  const [Cart, setCart] = useState(false);
   const callbacks = {
-    onDeleteItem: useCallback((code) => {
-      store.deleteItem(code);
+    onCartShow: useCallback(() => {
+      setCart(true);
+    }, [Cart]),
+
+    onAddGoods: useCallback((code) => {
+      store.onAddGoods(code);
     }, [store]),
 
-    onSelectItem: useCallback((code) => {
-      store.selectItem(code);
-    }, [store]),
-
-    onAddItem: useCallback(() => {
-      store.addItem();
+    onDeleteGoods: useCallback((code) => {
+      store.onDeleteGoods(code);
     }, [store])
-  }
+  };
 
   return (
-    <PageLayout>
-      <Head title='Приложение на чистом JS'/>
-      <Controls onAdd={callbacks.onAddItem}/>
-      <List list={list}
-            onDeleteItem={callbacks.onDeleteItem}
-            onSelectItem={callbacks.onSelectItem}/>
-    </PageLayout>
+    <>
+      <PageLayout>
+        <Head title='Магазин' />
+        <Controls
+          onCartClick={callbacks.onCartShow}
+          CartInfo={state.info}
+        />
+        <List list={state.list}
+          buttonsAction={callbacks.onAddGoods}
+          buttonsLabel="Добавить"
+        />
+      </PageLayout>
+
+      {
+        Cart ?
+          <CartModal list={state.Cart}
+            info={state.info}
+            onDelete={callbacks.onDeleteGoods}
+            setOpen={setCart}
+          />
+          : null
+      }
+    </>
   );
 }
 
